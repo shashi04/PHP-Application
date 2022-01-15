f7c71519ec508d508950dd4ff68fdac43bb7acf4
shashi04 <shashivardhan04@gmail.com>
2022-01-15 15:48:53 +0530

    PHP1

--- index.php
+++ index.php
@@ -1,10 +1,11 @@
 <?php
 
-// Comment these lines to hide errors
-error_reporting(E_ALL);
-ini_set('display_errors', 1);
 
-require 'includes/config.php';
-require 'includes/functions.php';
+include "header.php";
 
-init();
+include "body.php";
+include "newslettter.php";
+include "footer.php";
+?>
+		
+		
